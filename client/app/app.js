5a0200c11491e5f8a94fe8d43b57db88d82d0cfd
KalisaFalzone <kalisa.falzone@gmail.com>
2015-09-08 15:37:58 -0700

    [Save] adds landing page as a ui router state

--- client/app/app.js
+++ client/app/app.js
@@ -1,18 +1,28 @@
 var app = angular.module('myApp', [
   'ui.router',
-  'myApp.requestHoodServices', 
-  'myApp.map', 
+  'myApp.requestHoodServices',
+  'myApp.map',
   'myApp.results',
   'myApp.searchForm',
   'myApp.filter',
   'myApp.thumbnails'
-])
+]);
 console.log("app.js called");
 app.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.
-    otherwise("/");
+    otherwise("/landing");
 
   $stateProvider
+    .state('landing', {
+      url: "/landing",
+      controller: 'MainController',
+      controllerAs: 'main',
+      views: {
+        "": {
+          templateUrl: "./app/landing/landing.html"
+        }
+      }
+     })
     .state('main', {
       url: "/main",
       controller: 'MainController',
@@ -40,6 +50,6 @@ app.config(function($stateProvider, $urlRouterProvider) {
           templateUrl: "./app/details/detailsTemplate.html"
         }
       }
-    })
+    });
   });
 
